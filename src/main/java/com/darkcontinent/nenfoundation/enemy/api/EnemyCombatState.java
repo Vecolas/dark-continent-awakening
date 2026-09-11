9b2b24c43ac590d18bf0a35eba4b36c54b816cb2
Vecolas <alcy.neto110@gmail.com>
2026-09-11 20:10:37 -0300

    feat(enemy): add foundational enemy domain contracts

---
+++ src/main/java/com/darkcontinent/nenfoundation/enemy/api/EnemyCombatState.java
@@ -0,0 +1,5 @@
+package com.darkcontinent.nenfoundation.enemy.api;
+
+public enum EnemyCombatState {
+    IDLE, GROUP_ROAM, AGGRO, WINDUP, ACTIVE, RECOVERY, STAGGERED, RETREAT, DYING
+}
---
+++ src/main/java/com/darkcontinent/nenfoundation/enemy/api/FactionRelation.java
@@ -0,0 +1,3 @@
+package com.darkcontinent.nenfoundation.enemy.api;
+
+public enum FactionRelation { ALLY, NEUTRAL, PREY, HOSTILE }
